de3a638ee4f56120104e220276268bd9d2f54f57
laragrazzini <lgrazzini@udesa.edu.ar>
2023-11-01 15:41:12 -0300

    instalacion

---
+++ my-app/src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgzC3IIiOBHq3dpTf0i57Sb1rlWTHLElM",
+    authDomain: "proyectointegrador-7d512.firebaseapp.com",
+    projectId: "proyectointegrador-7d512",
+    storageBucket: "proyectointegrador-7d512.appspot.com",
+    messagingSenderId: "821387469543",
+    appId: "1:821387469543:web:37ef8c9d3d01eddda5b842",
+    measurementId: "G-SM4YZG7C1T"
+  };
