83a3c3c2d69298bfd8a611e1d09eb32b9c4c984b
SofiaArroyo <sarroyo@udesa.edu.ar>
2023-11-06 15:22:58 -0300

    Post, postform y home

---
+++ my-app/src/screens/Home/Home.js
@@ -0,0 +1,61 @@
+import react, { Component } from 'react';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
+import { db, auth } from '../../firebase/config';
+import PostForm from '../PostForm/PostForm';
+import Post from '../../components/Post/Post';
+
+class Home extends Component{
+    constructor(){
+        super()
+        this.state={
+            post:[]
+        }
+    }
+    componentDidMount(){
+        //Traer los datos de Firebase y cargarlos en el estado.
+        db.collection('posts').onSnapshot(
+            listaPosts => {
+                let postsAMostrar = [];
+
+                listaPosts.forEach(unPost => {
+                    postsAMostrar.push({
+                        id:unPost.id,
+                        datos: unPost.data()            
+                    })
+                })
+
+                this.setState({
+                    posts:postsAMostrar
+                })
+            }
+        )
+
+    }
+    logut(){
+        auth.signOut();
+    }
+    render(){
+        console.log(this.state);
+        return(
+            <View>
+                <Text>HOME</Text>
+                <TouchableOpacity onPress={()=>this.logout()}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
+               
+                <Text>Crear nuevo post</Text>
+                <PostForm />
+
+                <Text>Lista de posteos creados</Text>
+                
+                <FlatList
+                    data={this.state.posts}
+                    keyExtractor={ unPost => unPost.id }
+                    renderItem={ ({item}) => <Post dataPost = {item} />  }
+                />
+
+            </View>
+        )
+    }
+}
+export default Home;
---
+++ my-app/src/screens/PostForm/PostForm.js
@@ -0,0 +1,73 @@
+import react, { Component } from 'react';
+import {db, auth } from '../../firebase/config';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+
+class PostForm extends Component{
+    contructor(){
+        super()
+        this.state={
+            textoPost: "",
+        }
+    }
+    crearPost(owner, textoPost, createdAt){
+        //Crear la colección Users
+        db.collection('posts').add({
+            owner: auth.currentUser.email, //auth.currentUser.email,
+            textoPost: this.state.textoPost, //this.state.textoPost,
+            createdAt: Date.now(),//Date.now(), 
+            likes:[],
+            comentarios:[]
+        })
+        .then( res => console.log(res))
+        .catch( e => console.log(e))
+    }
+    render() {
+        return (
+          <View style={styles.formContainer}>
+            <Text style={styles.title}>New Post</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => this.setState({ textoPost: text })}
+              placeholder='Write a caption...'
+              keyboardType='default'
+              value={this.state.textoPost}
+            />
+            <TouchableOpacity style={styles.button} onPress={() => this.crearPost(auth.currentUser.email, this.state.textoPost, Date.now())}>
+              <Text style={styles.textButton}>Post</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      }
+}
+
+const styles = StyleSheet.create({
+    formContainer: {
+      paddingHorizontal: 20,
+      marginTop: 20,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    input: {
+      height: 40,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 8,
+      paddingLeft: 10,
+      marginBottom: 20,
+    },
+    button: {
+      backgroundColor: '#3498db',
+      padding: 15,
+      alignItems: 'center',
+      borderRadius: 8,
+    },
+    textButton: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+  });
+
+export default PostForm;
